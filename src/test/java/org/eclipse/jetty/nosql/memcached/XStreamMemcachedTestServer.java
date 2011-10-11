5766d41ba1e93e4b2fff942c54844af09640ed0d
Yamashita Yuu <yamashita@geishatokyo.com>
2011-10-12 04:02:34 +0900

    replaced test classes by test-memcached-sessions.

---
+++ src/test/java/org/eclipse/jetty/nosql/memcached/XStreamMemcachedTestServer.java
@@ -0,0 +1,70 @@
+package org.eclipse.jetty.nosql.memcached;
+
+// ========================================================================
+// Copyright (c) 1996-2009 Mort Bay Consulting Pty. Ltd.
+// ------------------------------------------------------------------------
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// and Apache License v2.0 which accompanies this distribution.
+// The Eclipse Public License is available at 
+// http://www.eclipse.org/legal/epl-v10.html
+// The Apache License v2.0 is available at
+// http://www.opensource.org/licenses/apache2.0.php
+// You may elect to redistribute this code under either of these licenses. 
+// ========================================================================
+
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.nosql.memcached.MemcachedSessionIdManager;
+import org.eclipse.jetty.nosql.memcached.MemcachedSessionManager;
+import org.eclipse.jetty.server.SessionIdManager;
+import org.eclipse.jetty.server.SessionManager;
+import org.eclipse.jetty.server.session.AbstractTestServer;
+import org.eclipse.jetty.server.session.SessionHandler;
+import org.eclipse.jetty.nosql.session.xstream.*;
+
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class XStreamMemcachedTestServer extends MemcachedTestServer
+{
+    public XStreamMemcachedTestServer(int port)
+    {
+        super(port);
+    }
+
+    public XStreamMemcachedTestServer(int port, int maxInactivePeriod, int scavengePeriod)
+    {
+        super(port, maxInactivePeriod, scavengePeriod);
+    }
+    
+    
+    public XStreamMemcachedTestServer(int port, int maxInactivePeriod, int scavengePeriod, boolean saveAllAttributes)
+    {
+        super(port, maxInactivePeriod, scavengePeriod, saveAllAttributes);
+    }
+
+    @Override
+    public SessionManager newSessionManager()
+    {
+        MemcachedSessionManager manager;
+        try
+        {
+            manager = new MemcachedSessionManager();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+        
+        manager.setSavePeriod(1);
+        manager.setStalePeriod(0);
+        manager.setSaveAllAttributes(_saveAllAttributes);
+        manager.setSessionFacade(new XStreamSessionFacade());
+        //manager.setScavengePeriod((int)TimeUnit.SECONDS.toMillis(_scavengePeriod));
+        return manager;
+    }
+}
